515876497509df88bb6cda1b060536f9f50c9685
Janaina <janainasoul@icloud.com>
2023-04-18 11:20:09 -0300

    commit inicial

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,13 @@
+import './Banner.css'
+
+function Banner (){
+    //JSX parece HTML mas não é
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="banner principal do Organo Ladies"/>
+        </header>
+
+    )
+}
+
+export default Banner
